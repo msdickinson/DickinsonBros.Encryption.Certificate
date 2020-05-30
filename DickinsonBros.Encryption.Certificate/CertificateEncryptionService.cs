df4c29cb8d5e597723bc4db2e0f604e8f3c1fbbc
Mark Dickinson <marksamdickinson@gmail.com>
2020-05-30 07:59:19 -0700

    Reduced Code

--- DickinsonBros.Encryption.Certificate/CertificateEncryptionService.cs
+++ DickinsonBros.Encryption.Certificate/CertificateEncryptionService.cs
@@ -25,26 +25,7 @@ namespace DickinsonBros.Encryption.Certificate
 
         public string Decrypt(string encrypted)
         {
-            try
-            {
-                using var x509Store = new X509Store(StoreName.My, _storeLocation);
-                x509Store.Open(OpenFlags.ReadOnly);
-                var certificateCollection = x509Store.Certificates.Find(X509FindType.FindByThumbprint, _thumbPrint, false);
-                if (certificateCollection.Count > 0)
-                {
-                    var certificate = certificateCollection[0];
-                    using var rsaPrivateKey = certificate.GetRSAPrivateKey();
-                    return Encoding.ASCII.GetString(rsaPrivateKey.Decrypt(Convert.FromBase64String(encrypted), RSAEncryptionPadding.Pkcs1));
-                }
-                else
-                {
-                    throw new Exception($"No certificate found for Thumbprint {_thumbPrint} in location {_storeLocation}");
-                }
-            }
-            catch (Exception ex)
-            {
-                throw new Exception($"Unhandled exception. Thumbprint: {_thumbPrint}, Location: {_storeLocation}", ex);
-            }
+            return Decrypt(Convert.FromBase64String(encrypted));
         }
 
         public string Decrypt(byte[] encrypted)
@@ -73,27 +54,7 @@ namespace DickinsonBros.Encryption.Certificate
 
         public string Encrypt(string unencrypted)
         {
-            try
-            {
-                using var x509Store = new X509Store(StoreName.My, _storeLocation);
-                x509Store.Open(OpenFlags.ReadOnly);
-                var certificateCollection = x509Store.Certificates.Find(X509FindType.FindByThumbprint, _thumbPrint, false);
-                if (certificateCollection.Count > 0)
-                {
-                    var certificate = certificateCollection[0];
-                    using RSA rsa = certificate.GetRSAPublicKey();
-                    byte[] bytestodecrypt = Encoding.UTF8.GetBytes(unencrypted);
-                    return Convert.ToBase64String(rsa.Encrypt(bytestodecrypt, RSAEncryptionPadding.Pkcs1));
-                }
-                else
-                {
-                    throw new Exception($"No certificate found for Thumbprint {_thumbPrint} in location {_storeLocation}");
-                }
-            }
-            catch (Exception ex)
-            {
-                throw new Exception($"Unhandled exception. Thumbprint: {_thumbPrint}, Location: {_storeLocation}", ex);
-            }
+            return Convert.ToBase64String(EncryptToByteArray(unencrypted));
         }
 
         public byte[] EncryptToByteArray(string unencrypted)
